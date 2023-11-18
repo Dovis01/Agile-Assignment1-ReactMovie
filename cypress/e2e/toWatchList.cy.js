81408aa04b3f8c038f52aa67ca9e9d3830a56db0
Jiahan Chen <zsj1364226740@gmail.com>
2023-11-18 08:29:41 +0800

    Tested-toWatchList Page and delete other test cases

--- cypress/e2e/toWatchList.cy.js
+++ cypress/e2e/toWatchList.cy.js
@@ -1,10 +1,10 @@
 let movies;
 const movieId = 497582; // Enola Holmes movie id
 
-describe("The favourites feature", () => {
+describe("The toWatchList feature", () => {
     before(() => {
         cy.request(
-            `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+            `https://api.themoviedb.org/3/movie/upcoming?api_key=${Cypress.env(
                 "TMDB_KEY"
             )}&language=en-US&include_adult=false&include_video=false&page=1`
         )
@@ -14,23 +14,23 @@ describe("The favourites feature", () => {
             });
     });
     beforeEach(() => {
-        cy.visit("/");
+        cy.visit("/movies/upcoming");
     });
 
-    describe("Selecting favourites", () => {
-        it("selected movie card shows the red heart", () => {
+    describe("Selecting movies to watch", () => {
+        it("selected movie card shows the green avatar", () => {
             cy.get(".MuiCardHeader-root").eq(1).find("svg").should("not.exist");
-            cy.get("button[aria-label='add to favorites']").eq(1).click();
+            cy.get("button[aria-label='add to watch']").eq(1).click();
             cy.get(".MuiCardHeader-root").eq(1).find("svg");
         });
     });
 
-    describe("The favourites page", () => {
+    describe("The to watch list page", () => {
         beforeEach(() => {
             // Select two favourites and navigate to Favourites page
-            cy.get("button[aria-label='add to favorites']").eq(1).click();
-            cy.get("button[aria-label='add to favorites']").eq(3).click();
-            cy.get("button").contains("Favorites").click();
+            cy.get("button[aria-label='add to watch']").eq(1).click();
+            cy.get("button[aria-label='add to watch']").eq(3).click();
+            cy.get("button").contains("ToWatchList").click();
         });
         it("only the tagged movies are listed", () => {
             cy.get(".MuiCardHeader-content").should("have.length", 2);
